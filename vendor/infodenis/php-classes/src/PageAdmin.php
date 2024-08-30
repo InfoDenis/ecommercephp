ccadad08aa93e49050489250c72f1785cb3809f6
InfoDenis <mensagemparavoce_54@hotmail.com>
2024-08-30 20:46:32 -0300

    Construção e configuração de rotas, da classe e do template PageAdmin.

---
+++ vendor/infodenis/php-classes/src/PageAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InfoDenis;
+
+class PageAdmin extends Page {
+
+  public function __construct($opts = array(), $tplDir = "/views/admin/")
+  {
+
+    parent::__construct($opts, $tplDir);
+
+  }
+
+}
